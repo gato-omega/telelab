3fe540e451a40cf4d67a9fe5500475fe1606046e
gato_omega <gato_omega@hotmail.com>
2011-04-08 17:29:47 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telelab02::Application.config.secret_token = '9ecaeb681bf238e68e71c1177d20a4b4b008aa3c76d879edb28594c8bdf8128a463c65ea8ed6bf629c6c9500b80e179b4a77a394692c0a4286b6801f20c3d570'
