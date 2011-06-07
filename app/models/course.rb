8d7c0f14848ecb16a3b7fa500e7f8211e1089153
gato_omega <gato_omega@hotmail.com>
2011-06-07 12:00:15 -0500

    added relationships, not migrations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ActiveRecord::Base
   attr_accessible :name, :description, :hashed_password, :options
+  has_and_belongs_to_many :teachers
+  has_and_belongs_to_many :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,3 @@
 class Student < User
-  
+  has_and_belongs_to_many :courses
 end
