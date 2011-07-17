8fea8b35ec8c14189fa7b9c1292fafcb26926ba9
gato_omega <gato_omega@hotmail.com>
2011-07-17 17:21:11 -0500

    admin-teachers with datatable ok

--- public/javascripts/teachers.js
+++ public/javascripts/teachers.js
@@ -1,11 +1,8 @@
 //Add datatables
 $(document).ready(function() {
-	$('#teachers_table').dataTable();
-} );
-
-/*
- {
-		"sScrollY": 200,
-		"bJQueryUI": true,
-		"sPaginationType": "full_numbers"
-	}
+    $('#teachers_table').dataTable({
+        "sScrollY": 200,
+        "bJQueryUI": true,
+        "sPaginationType": "full_numbers"
+    });
+});
