536ba91b4764edeb33b1d9e029014175029a6274
gacollazos <gedres8@hotmail.com>
2011-08-26 18:34:35 -0500

    changed reservas by practicas, set token_input for practicas

--- app/helpers/practica_helper.rb
+++
@@ -1,8 +0,0 @@
-module PracticaHelper
-  def chat(channel, &block)
-    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse(FAYE_SERVER_URL)
-    Net::HTTP.post_form(uri, :message => message.to_json)
-    nil
-  end
-end
