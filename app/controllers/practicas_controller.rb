39a26312e9a7fdbac650456170cfee1cf60a983a
gato_omega <gato_omega@hotmail.com>
2011-09-05 18:15:05 -0500

    REST routes totally RESTful compatable, for every resource like users, etc, eliminated commented code, moved tokeninput json response to users controller

--- app/controllers/practicas_controller.rb
+++ app/controllers/practicas_controller.rb
@@ -1,6 +1,5 @@
 class PracticasController < ApplicationController
-  # GET /practicas
-  # GET /practicas.xml
+
   def index
     @practicas = Practica.all
 
@@ -10,8 +9,6 @@ class PracticasController < ApplicationController
     end
   end
 
-    # GET /practicas/1
-    # GET /practicas/1.xml
   def show
     @practica = Practica.find(params[:id])
     @dispositivos = @practica.dispositivos
@@ -23,8 +20,6 @@ class PracticasController < ApplicationController
     end
   end
 
-    # GET /practicas/new
-    # GET /practicas/new.xml
   def new
     @practica = Practica.new
     @dispositivos = Dispositivo.all
@@ -37,7 +32,6 @@ class PracticasController < ApplicationController
     end
   end
 
-    # GET /practicas/1/edit
   def edit
     @practica = Practica.find(params[:id])
     @dispositivos = Dispositivo.all
@@ -45,8 +39,6 @@ class PracticasController < ApplicationController
     @allowed_users = @practica.users
   end
 
-    # POST /practicas
-    # POST /practicas.xml
   def create
     @practica = Practica.new(params[:practica])
 
@@ -61,8 +53,6 @@ class PracticasController < ApplicationController
     end
   end
 
-    # PUT /practicas/1
-    # PUT /practicas/1.xml
   def update
     params[:practica][:dispositivo_ids] ||= []
     @practica = Practica.find(params[:id])
@@ -78,8 +68,6 @@ class PracticasController < ApplicationController
     end
   end
 
-    # DELETE /practicas/1
-    # DELETE /practicas/1.xml
   def destroy
     @practica = Practica.find(params[:id])
     @practica.destroy
@@ -95,74 +83,5 @@ class PracticasController < ApplicationController
     @dispositivos_reservados = @practica.dispositivos
     @allowed_users = @practica.users
   end
-
-    ##################### add from reservas_controller old ########################################
-
-  def json_users
-    @all_users = User.where("username like ?", "%#{params[:q]}%")
-
-    respond_to do |format|
-      format.json { render :json => @all_users.collect { |user| {:id => user.id, :name => user.username} } }
-    end
-  end
-
-    ############### add from practicas_controller old ############################
-
-  #before_filter :load_bot, :only => [:ircchat]
-
-  def message
-
-    #Get the message and channel
-    @message = params[:message]
-    @channel = params[:channel]
-
-    bot_manager=GBotManager.instance
-    @bot=bot_manager.load_bot_for current_user
-
-
-    if @channel.nil?
-      @channel = bot_manager.config[:client][:default_channels].first
-      logger.debug "###########################  NOW channel is #{@channel}"
-    end
-
-    logger.debug "###########################  channel is #{@channel}"
-    logger.debug "###########################  message is #{@message}"
-
-    @bot.action(@channel, @message)
-
-    respond_to :js
-
-  end
-
-
-  #def ASI_NO_SELLAAMmessage
-  #
-  #  #Get the message and channel
-  #  @message = params[:message]
-  #  @channel = params[:channel]
-  #
-  #  if @channel.nil?
-  #    @channel = FAYE_DEFAULT_CHANNEL
-  #    logger.debug "###########################  NIL channel is #{@channel}"
-  #  end
-  #
-  #  @channel = "#{FAYE_CHANNEL_PREFIX}#{@channel}"
-  #
-  #  if current_user
-  #    @faye_user_channel = current_user.username
-  #    @faye_practice_channel = 'lobby'
-  #  else
-  #    @faye_user_channel = "lobby"
-  #    @faye_practice_channel = 'lobby'
-  #  end
-  #
-  #  renders message.js.erb
-  #
-  #  logger.debug "###########################  channel is #{@channel}"
-  #  logger.debug "###########################  message is #{@message}"
-  #
-  #  respond_to :js
-  #
-  #end
 
 end
