c96adf6cfa1fc9c2986e2dc28ebef22ead60a2b9
gato_omega <gato_omega@hotmail.com>
2011-09-07 19:48:06 -0500

    first attempt UI

---
+++ public/javascripts/application.js
@@ -0,0 +1,12 @@
+$(function() {
+    // Initialize visual things
+
+    $( "a.button" ).button();
+
+    // Visual CRUD icons
+    $('a.new_button').html("<img src='/images/icons/new.png'>")
+    $('a.edit_button').html("<img src='/images/icons/edit.png'>")
+    $('a.show_button').html("<img src='/images/icons/show.png'>")
+    $('a.delete_button').html("<img src='/images/icons/delete.png'>")
+
+});
--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,7 +1,6 @@
 //Add datatables
 $(document).ready(function() {
     $('#users_table').dataTable({
-        "sScrollY": 200,
         "bJQueryUI": true,
         "sPaginationType": "full_numbers"
     });
